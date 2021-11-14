0ce965082efe5c622fd33859d1ab3bc4caeb45f7
Albertopostani <91824639+Albertopostani@users.noreply.github.com>
2021-11-14 20:19:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int i=0,n=0,primo=1,secondo=0,risultato;
+    cin>>n;
+    if(n>=2){
+        for(int i=1;i<=n;i++){
+            risultato=primo+secondo;
+            primo=secondo;
+            secondo=risultato;
+            cout<<risultato<<endl;
+        }
+    }else{
+        cout<<"errore"<<endl;
+    }
+return 0;
 }
